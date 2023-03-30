b361a0ae6840fae9eca0d05d070be366b71e7b18
Piotr Skrobol <pio.skro@gmail.com>
2023-03-30 22:35:36 +0200

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Taboritis\Wrapper;
+
+/**
+ * @covers nothing
+ */
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
